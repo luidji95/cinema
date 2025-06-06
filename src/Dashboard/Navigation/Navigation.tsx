984cf633644a7308bf3d7fcbda2c13ca2953399c
Milos Petrovic <milos.petrovic09.95@gmail.com>
2025-06-06 23:34:22 +0200

    navigation styling

--- src/Dashboard/Navigation/Navigation.tsx
+++ src/Dashboard/Navigation/Navigation.tsx
@@ -4,7 +4,12 @@ const Navigation = () => {
   return (
     <div className="navigation-main">
       <div className="navigation-left">
-        <img src="logo"></img>
+        <img
+          src="https://vihdejdhouasksfmldlv.supabase.co/storage/v1/object/sign/logo-image/movie-logo.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InN0b3JhZ2UtdXJsLXNpZ25pbmcta2V5X2JiNzVhNTdjLWI5ZTItNGEyMC04NjY3LTA4NjY4NzI1MmYwYyJ9.eyJ1cmwiOiJsb2dvLWltYWdlL21vdmllLWxvZ28ucG5nIiwiaWF0IjoxNzQ4MjkwNDg4LCJleHAiOjE3Nzk4MjY0ODh9.Hu1Rxjz0j77UAQYR2Z8YsXUhAkCYXoBJBt19ZdnxYmI"
+          alt="Logo"
+          className="logo"
+        />
+        <p>cinema 100</p>
       </div>
       <div className="navigation-right">
         <p>Welcome back name</p>
