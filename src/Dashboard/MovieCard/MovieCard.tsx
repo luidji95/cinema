9da774f0e6fee773561e18463dc3c28fe802db60
Milos Petrovic <milos.petrovic09.95@gmail.com>
2025-07-26 23:10:53 +0200

    add bokmark icon ribbon to MovieCard overlay

--- src/Dashboard/MovieCard/MovieCard.tsx
+++ src/Dashboard/MovieCard/MovieCard.tsx
@@ -24,7 +24,9 @@ const MovieCard = ({
 
         <div className="overlay">
           <button className="see-more-btn">See more</button>
-          <button className="fav-btn">♡</button>
+          <div className="ribbon">
+            <img src="https://vihdejdhouasksfmldlv.supabase.co/storage/v1/object/sign/bookmarked/ribbon.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9iYjc1YTU3Yy1iOWUyLTRhMjAtODY2Ny0wODY2ODcyNTJmMGMiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJib29rbWFya2VkL3JpYmJvbi5wbmciLCJpYXQiOjE3NTM1NjAxMzIsImV4cCI6MTc4NTA5NjEzMn0.eLcvuJxSNnlh-nIfBUryqgdUuMz-vjg7MvKTfkAak6k" />
+          </div>
         </div>
       </div>
 
@@ -37,5 +39,3 @@ const MovieCard = ({
 };
 
 export default MovieCard;
-
-
