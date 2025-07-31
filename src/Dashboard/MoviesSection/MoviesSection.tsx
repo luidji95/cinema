4363b8f709000bfea1df36a63c46990d67a4bbe3
Milos Petrovic <milos.petrovic09.95@gmail.com>
2025-07-31 22:18:05 +0200

    pagination changes

--- src/Dashboard/MoviesSection/MoviesSection.tsx
+++ src/Dashboard/MoviesSection/MoviesSection.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import { supabase } from "../../Supabase/supabaseClient";
 import MoviesGrid from "../MoviesGrid/MoviesGrid";
 import Pagination from "../Pagination/pagination";
-// import { singleMovie } from "../../MoviesData/dataMovies";
+import type { singleMovie } from "../../MoviesData/dataMovies";
 
 type Props = {
   perPage: number;
@@ -12,9 +12,7 @@ const MoviesSection = ({ perPage }: Props) => {
   const [movies, setMovies] = useState<singleMovie[]>([]);
   const [totalMovies, setTotalMovies] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
-  const [loading, setLoading] = useState(true);
-
-  const sectionRef = useRef<HTMLDivElement | null>(null);
+  const [loading, setLoading] = useState(false);
 
   const totalPages = Math.ceil(totalMovies / perPage);
 
@@ -38,58 +36,18 @@ const MoviesSection = ({ perPage }: Props) => {
     fetchMovies();
   }, [currentPage, perPage]);
 
-  useEffect(() => {
-    if (!loading) {
-      const images = sectionRef.current?.querySelectorAll("img") || [];
-      let loadedCount = 0;
-
-      images.forEach((img) => {
-        if (img.complete) {
-          loadedCount++;
-        } else {
-          img.addEventListener("load", handleLoad);
-          img.addEventListener("error", handleLoad);
-        }
-      });
-
-      function handleLoad() {
-        loadedCount++;
-        if (loadedCount === images.length) {
-          requestAnimationFrame(() => {
-            sectionRef.current?.scrollIntoView({
-              behavior: "smooth",
-              block: "end",
-            });
-          });
-        }
-      }
-
-      return () => {
-        images.forEach((img) => {
-          img.removeEventListener("load", handleLoad);
-          img.removeEventListener("error", handleLoad);
-        });
-      };
-    }
-  }, [loading, currentPage]);
-
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page);
-  };
-
   return (
-    <div ref={sectionRef}>
+    <div>
       {loading ? (
         <p className="min-height">Učitavanje filmova...</p>
       ) : (
-        <>
-          <MoviesGrid movies={movies} />
-        </>
+        <MoviesGrid movies={movies} />
       )}
+
       <Pagination
         currentPage={currentPage}
         totalPages={totalPages}
-        onPageChange={handlePageChange}
+        onPageChange={setCurrentPage}
       />
     </div>
   );
